e49eadd14e1090723e1c0fe1c630b8adca13fac1
Carlos Eduardo Rangel Teixeira <certeixeira@outlook.com>
2021-12-11 19:05:11 -0300

    iniciando o recycleview

--- app/src/main/java/com/example/listadeprodutos/MainActivity.kt
+++ app/src/main/java/com/example/listadeprodutos/MainActivity.kt
@@ -10,5 +10,11 @@ class MainActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nome: TextView = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de Frutas"
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, manga e maçã"
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "19.99"
     }
 }
